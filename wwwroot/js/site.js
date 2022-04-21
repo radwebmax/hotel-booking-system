21cef1cd240476418577f3485480534a10e2a898
AlikLike <aliyarabdullayev@gmail.com>
2022-04-22 01:40:43 +0800

    new_www

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//Access to reservation managment
+let manage_reseravtions = document.querySelector('.manage-reseravtions');
 
-// Write your JavaScript code.
+
+const checkAdmin = event => {
+    event.preventDefault();
+    let answer = prompt('Enter an admin password to manage reservations:');
+    //Password: 1111
+    if (answer == '1111') {
+        location.href = '/Reservation/Index';
+    } else {
+        alert('Sorry, password is wrong!');
+    }
+}
+
+
+manage_reseravtions.addEventListener('click', checkAdmin);
+
+document.querySelectorAll('.hasDatepicker').forEach(el => {
+    el.addEventListener('input', () => {
+        el.value = `${el.value} 00:00:00`;
+        console.log(el.value);
+    })
+})
+//FormData
