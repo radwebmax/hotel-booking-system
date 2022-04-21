e4f9d39c13dfdd03c66488d94e3d3f0b6737d701
AlikLike <aliyarabdullayev@gmail.com>
2022-04-22 01:28:21 +0800

    wwwroot new

---
+++ wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿//Access to reservation managment
+let manage_reseravtions = document.querySelector('.manage-reseravtions');
+
+
+const checkAdmin = event => {
+    event.preventDefault();
+    let answer = prompt('Enter an admin password to manage reservations:');
+    //Password: 1111
+    if (answer == '1111') {
+        location.href = '/Reservation/Index';
+    } else {
+        alert('Sorry, password is wrong!');
+    }
+}
+
+
+manage_reseravtions.addEventListener('click', checkAdmin);
+
+document.querySelectorAll('.hasDatepicker').forEach(el => {
+    el.addEventListener('input', () => {
+        el.value = `${el.value} 00:00:00`;
+        console.log(el.value);
+    })
+})
+//FormData
