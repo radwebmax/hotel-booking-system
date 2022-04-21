789d671242f8ef0d1523040ec2c3929132b80ef8
Mr-Pain-Jr <mrpainjr@gmail.com>
2022-04-21 20:15:08 +0300

    added migration

---
+++ Data/Migrations/20220418222951_Reservation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelReservationSystem.Data.Migrations
+{
+    public partial class Reservation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reservation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ArrivalDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DepartureDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Room = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    RoomPreference = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    GuestsNumber = table.Column<int>(type: "int", nullable: false),
+                    ChildrenNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservation", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservation");
+        }
+    }
+}
